43f81314f33fa24ef4469ebf8996d87499bdaf7c
Pat Sissons <pat@marinels.com>
2016-07-18 09:50:46 -0700

    updated typings for the timeline component

--- vs.d.ts
+++ vs.d.ts
@@ -5,139 +5,13 @@
 // Definitions: ---
 
 declare namespace __vis {
-  export interface DataItem {
-    className?: string;
-    content: string;
-    end?: Date | number | string | moment.Moment;
-    group?: any;
-    id?: string | number;
-    start: Date | number | string | moment.Moment;
-    style?: string;
-    subgroup?: string | number;
-    title?: string;
-    type?: string;
-    editable?: boolean;
-  }
-
-  export interface DataGroup {
-    className?: string;
-    content: string;
-    id: string | number;
-    style?: string;
-    subgroup?: string | number;
-    title?: string;
-  }
-
-  export class DataSet<T extends DataItem | DataGroup> {
-    constructor(items: Array<T>);
-  }
-
-  export class DataView<T extends DataItem | DataGroup> {
-    constructor(items: Array<T>);
-  }
-
-  export interface TimelineEditableOption {
-    add?: boolean;
-    remove?: boolean;
-    updateGroup?: boolean;
-    updateTime?: boolean;
-  }
-
-  export interface TimelineGroupEditableOption {
-    add?: boolean;
-    remove?: boolean;
-    order?: boolean;
-  }
-
-  export interface TimelineMarginOption {
-    axis?: number;
-    item?: number | { horizontal?: number; vertical?: number; };
-  }
-
-  export interface TimelineOrientationOption {
-    axis?: string;
-    item?: string;
-  }
-
-  export interface TimelineTimeAxisOption {
-    scale?: 'millisecond' | 'second' | 'minute' | 'hour' | 'weekday' | 'day' | 'month' | 'year';
-    step?: number;
-  }
-
-  export interface TimelineOptions {
-    align?: string;
-    autoResize?: boolean;
-    clickToUse?: boolean;
-    configure?: boolean | ((option: string, path: Array<string>) => boolean);
-    dataAttributes?: boolean | string | string[];
-    editable?: boolean | TimelineEditableOption;
-    end?: Date | number | string | moment.Moment;
-    format?: any;
-    groupEditable?: boolean | TimelineGroupEditableOption;
-    groupOrder?: string | Function; // TODO
-    groupOrderSwap?: (fromGroup: any, toGroup: any, groups: DataSet<DataGroup>) => void;
-    groupTemplate?: Function; // TODO
-    height?: number | string;
-    hiddenDates?: any; // TODO
-    itemsAlwaysDraggable?: boolean;
-    locale?: string;
-    locales?: any; // TODO
-    moment?: Function; // TODO
-    margin?: number | TimelineMarginOption;
-    max?: Date | number | string | moment.Moment;
-    maxHeight?: number | string;
-    maxMinorChars?: number;
-    min?: Date | number | string | moment.Moment;
-    minHeight?: number | string;
-    moveable?: boolean;
-    multiselect?: boolean;
-    multiselectPerGroup?: boolean;
-    onAdd?: Function; // TODO
-    onAddGroup?: Function; // TODO
-    onUpdate?: Function; // TODO
-    onMove?: Function; // TODO
-    onMoveGroup?: Function; // TODO
-    onMoving?: Function; // TODO
-    onRemove?: Function; // TODO
-    onRemoveGroup?: Function; // TODO
-    order?: Function; // TODO
-    orientation?: string | TimelineOrientationOption;
-    selectable?: boolean;
-    showCurrentTime?: boolean;
-    showMajorLabels?: boolean;
-    showMinorLabels?: boolean;
-    stack?: boolean;
-    snap?: (date: Date, scale: string, step: number) => Date | number;
-    start?: Date | number | string | moment.Moment;
-    template?: Function; // TODO
-    throttleRedraw?: number;
-    timeAxis?: TimelineTimeAxisOption;
-    type?: string;
-    width?: string | number;
-    zoomable?: boolean;
-    zoomKey?: string;
-    zoomMax?: number;
-    zoomMin?: number;
-  }
-
-  export interface TimelineFitOptions {
-    animation?: boolean | { duration?: number; easingFunction?: string };
-  }
-
-  export interface TimelineEventPropertiesResult {
-    group?: number;
-    item?: number;
-    pageX: number;
-    pageY: number;
-    x: number;
-    y: number;
-    time: Date;
-    snappedTime: Date;
-    what?: 'item' | 'background' | 'axis' | 'group-label' | 'custom-time' | 'current-time';
-    event: Event;
-  }
-
-  export type TimelineEvents =
+  type IdType = string | number;
+  type SubgroupType = IdType;
+  type DateType = Date | number | string | moment.Moment;
+  type HeightWidthType = IdType;
+  type TimelineTimeAxisScaleType = 'millisecond' | 'second' | 'minute' | 'hour' | 'weekday' | 'day' | 'month' | 'year';
+  type TimelineEventPropertiesResultWhatType = 'item' | 'background' | 'axis' | 'group-label' | 'custom-time' | 'current-time';
+  type TimelineEvents =
     'currentTimeTick' |
     'click' |
     'contextmenu' |
@@ -150,46 +24,211 @@ declare namespace __vis {
     'timechange' |
     'timechanged';
 
+  interface DataItem {
+    className?: string;
+    content: string;
+    end?: DateType;
+    group?: any;
+    id?: IdType;
+    start: DateType;
+    style?: string;
+    subgroup?: SubgroupType;
+    title?: string;
+    type?: string;
+    editable?: boolean;
+  }
+
+  interface DataGroup {
+    className?: string;
+    content: string;
+    id: IdType;
+    style?: string;
+    subgroup?: SubgroupType;
+    title?: string;
+  }
+
+  interface TimelineEditableOption {
+    add?: boolean;
+    remove?: boolean;
+    updateGroup?: boolean;
+    updateTime?: boolean;
+  }
+
+  interface TimelineGroupEditableOption {
+    add?: boolean;
+    remove?: boolean;
+    order?: boolean;
+  }
+
+  interface TimelineMarginItem {
+    horizontal?: number;
+    vertical?: number;
+  }
+
+  type TimelineMarginItemType = number | TimelineMarginItem;
+
+  interface TimelineMarginOption {
+    axis?: number;
+    item?: TimelineMarginItemType;
+  }
+
+  interface TimelineOrientationOption {
+    axis?: string;
+    item?: string;
+  }
+
+  interface TimelineTimeAxisOption {
+    scale?: TimelineTimeAxisScaleType;
+    step?: number;
+  }
+
+  type TimelineOptionsConfigureFunction = (option: string, path: Array<string>) => boolean;
+  type TimelineOptionsConfigureType = boolean | TimelineOptionsConfigureFunction;
+  type TimelineOptionsDataAttributesType = boolean | string | string[];
+  type TimelineOptionsEditableType = boolean | TimelineEditableOption;
+  type TimelineOptionsGroupEditableType = boolean | TimelineGroupEditableOption;
+  type TimelineOptionsGroupOrderType = string | Function; // TODO
+  type TimelineOptionsGroupOrderSwapFunction = (fromGroup: any, toGroup: any, groups: DataSet<DataGroup>) => void;
+  type TimelineOptionsMarginType = number | TimelineMarginOption;
+  type TimelineOptionsOrientationType = string | TimelineOrientationOption;
+  type TimelineOptionsSnapFunction = (date: Date, scale: string, step: number) => Date | number;
+  // type TimelineOptions =
+
+  interface TimelineOptions {
+    align?: string;
+    autoResize?: boolean;
+    clickToUse?: boolean;
+    configure?: TimelineOptionsConfigureType;
+    dataAttributes?: TimelineOptionsDataAttributesType;
+    editable?: TimelineOptionsEditableType;
+    end?: DateType;
+    format?: any; // TODO
+    groupEditable?: TimelineOptionsGroupEditableType;
+    groupOrder?: TimelineOptionsGroupOrderType;
+    groupOrderSwap?: TimelineOptionsGroupOrderSwapFunction;
+    groupTemplate?: Function; // TODO
+    height?: HeightWidthType;
+    hiddenDates?: any; // TODO
+    itemsAlwaysDraggable?: boolean;
+    locale?: string;
+    locales?: any; // TODO
+    moment?: Function; // TODO
+    margin?: TimelineOptionsMarginType;
+    max?: DateType;
+    maxHeight?: HeightWidthType;
+    maxMinorChars?: number;
+    min?: DateType;
+    minHeight?: HeightWidthType;
+    moveable?: boolean;
+    multiselect?: boolean;
+    multiselectPerGroup?: boolean;
+    onAdd?: Function; // TODO
+    onAddGroup?: Function; // TODO
+    onUpdate?: Function; // TODO
+    onMove?: Function; // TODO
+    onMoveGroup?: Function; // TODO
+    onMoving?: Function; // TODO
+    onRemove?: Function; // TODO
+    onRemoveGroup?: Function; // TODO
+    order?: Function; // TODO
+    orientation?: TimelineOptionsOrientationType;
+    selectable?: boolean;
+    showCurrentTime?: boolean;
+    showMajorLabels?: boolean;
+    showMinorLabels?: boolean;
+    stack?: boolean;
+    snap?: TimelineOptionsSnapFunction;
+    start?: DateType;
+    template?: Function; // TODO
+    throttleRedraw?: number;
+    timeAxis?: TimelineTimeAxisOption;
+    type?: string;
+    width?: HeightWidthType;
+    zoomable?: boolean;
+    zoomKey?: string;
+    zoomMax?: number;
+    zoomMin?: number;
+  }
+
+  interface TimelineFitAnimation {
+    duration?: number;
+    easingFunction?: string;
+  }
+
+  type TimelineFitAnimationType = boolean | TimelineFitAnimation;
+
+  interface TimelineFitOptions {
+    animation?: TimelineFitAnimationType;
+  }
+
+  interface TimelineEventPropertiesResult {
+    group?: number;
+    item?: number;
+    pageX: number;
+    pageY: number;
+    x: number;
+    y: number;
+    time: Date;
+    snappedTime: Date;
+    what?: TimelineEventPropertiesResultWhatType;
+    event: Event;
+  }
+
+  export class DataSet<T extends DataItem | DataGroup> {
+    constructor(items: Array<T>);
+
+    length: number;
+  }
+
+  export class DataView<T extends DataItem | DataGroup> {
+    constructor(items: Array<T>);
+
+    length: number;
+  }
+
+  type DataItemCollectionType = Array<DataItem> | DataSet<DataItem> | DataView<DataItem>;
+  type DataGroupCollectionType = Array<DataGroup> | DataSet<DataGroup> | DataView<DataGroup>;
+
   export class Timeline {
     constructor(
       container: HTMLElement,
-      items: Array<DataItem> | DataSet<DataItem> | DataView<DataItem>,
-      groups: Array<DataGroup> | DataSet<DataGroup> | DataView<DataGroup>,
+      items: DataItemCollectionType,
+      groups: DataGroupCollectionType,
       options?: TimelineOptions
     );
     constructor(
       container: HTMLElement,
-      items: Array<DataItem> | DataSet<DataItem> | DataView<DataItem>,
+      items: DataItemCollectionType,
       options?: TimelineOptions
     );
 
-    addCustomTime(time: Date | number | string | moment.Moment, id?: number | string): number | string;
+    addCustomTime(time: DateType, id?: IdType): IdType;
     destroy(): void;
     fit(options?: TimelineFitOptions): void;
-    focus(id: string | number, options?: TimelineFitOptions): void;
-    focus(ids: Array<string | number>, options?: TimelineFitOptions): void;
+    focus(id: IdType, options?: TimelineFitOptions): void;
+    focus(ids: Array<IdType>, options?: TimelineFitOptions): void;
     getCurrentTime(): Date;
-    getCustomTime(id?: string | number): Date;
+    getCustomTime(id?: IdType): Date;
     getEventProperties(event: Event): TimelineEventPropertiesResult;
     getItemRange(): any; // TODO
-    getSelection(): Array<number>;
-    getVisibleItems(): Array<number>;
+    getSelection(): Array<IdType>;
+    getVisibleItems(): Array<IdType>;
     getWindow(): Window;
-    moveTo(time: Date | number | string | moment.Moment, options?: TimelineFitOptions): void;
+    moveTo(time: DateType, options?: TimelineFitOptions): void;
     on(event: TimelineEvents, callback: Function): void;
     off(event: TimelineEvents, callback: Function): void;
     redraw(): void;
-    removeCustomTime(id: string | number): void;
-    setCurrentTime(time: Date | number | string | moment.Moment): void;
-    setCustomTime(time: Date | number | string | moment.Moment, id?: string | number): void;
-    setCustomTimeTitle(title: string, id?: string | number): void;
-    setData(data: { groups?: Array<DataGroup> | DataSet<DataGroup> | DataView<DataGroup>; items?: Array<DataItem> | DataSet<DataItem> | DataView<DataItem> }): void;
-    setGroups(groups?: Array<DataGroup> | DataSet<DataGroup> | DataView<DataGroup>): void;
-    setItems(items: Array<DataItem> | DataSet<DataItem> | DataView<DataItem>): void;
+    removeCustomTime(id: IdType): void;
+    setCurrentTime(time: DateType): void;
+    setCustomTime(time: DateType, id?: IdType): void;
+    setCustomTimeTitle(title: string, id?: IdType): void;
+    setData(data: { groups?: DataGroupCollectionType; items?: DataItemCollectionType }): void;
+    setGroups(groups?: DataGroupCollectionType): void;
+    setItems(items: DataItemCollectionType): void;
     setOptions(options: TimelineOptions): void;
-    setSelection(id: string | number): void;
-    setSelection(ids: Array<string | number>): void;
-    setWindow(start: Date | number | string | moment.Moment, end: Date | number | string | moment.Moment, options?: TimelineFitOptions): void;
+    setSelection(id: IdType): void;
+    setSelection(ids: Array<IdType>): void;
+    setWindow(start: DateType, end: DateType, options?: TimelineFitOptions): void;
   }
 
   export interface ITimelineStatic {
